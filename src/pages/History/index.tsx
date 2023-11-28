301c6cb01edbbd764467492540c37400c5c38f41
Clésar Sapelli Junior <clesarsapellijunior@gmail.com>
2023-11-28 16:04:41 -0300

    [UPDATE] Componente History

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,31 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
-  return <></>
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa teste</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 30 minutos</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
