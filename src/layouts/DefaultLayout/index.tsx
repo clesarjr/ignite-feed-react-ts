658d3dce65ab65e030d48f1f73a69b6ef5ee1018
Clésar Sapelli Junior <clesarsapellijunior@gmail.com>
2023-11-27 22:46:16 -0300

    [UPDATE] Componente Home

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../Components/Header'
-import { LayoutContainer } from './style'
+import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
   return (
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,52 @@
+import { Play } from 'phosphor-react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  StartCountDownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            type="text"
+            id="task"
+            list="task-suggestions"
+            placeholder="Nome do projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos</span>
+        </FormContainer>
+
+        <CountDownContainer></CountDownContainer>
+
+        <StartCountDownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountDownButton>
+      </form>
+    </HomeContainer>
+  )
+}
